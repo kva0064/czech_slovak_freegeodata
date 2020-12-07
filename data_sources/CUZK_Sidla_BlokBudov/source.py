fa2ae350bed65eee2400e6f72a24726bdc18096f
MichaelaBozonova <62642456+MichaelaBozonova@users.noreply.github.com>
2020-12-07 11:59:52 +0100

    Add files via upload
    Add CUZK - Sidla - BlokBudov

---
+++ data_sources/CUZK_Sidla_BlokBudov/source.py
@@ -0,0 +1,19 @@
+from .. source import Source
+import os
+from qgis.core import QgsVectorLayer, QgsMessageLog
+
+class BlokBudov(Source):
+    def get_vector(self, extent, EPSG):
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'BlokBudov.shp')
+        # TODO read data from HTTP source join with geodata, etc.
+        vector = QgsVectorLayer(path, "Blok budov", "ogr")
+        vector.loadNamedStyle(os.path.dirname(__file__) + '/data/styleBlokBudov.qml')
+        if not vector.isValid():
+            QgsMessageLog.logMessage("Vrstvu " + path + " se nepodařilo načíst", "GeoData")
+            return None
+        else:
+            return vector
+
+    def get_raster(self, extent, EPSG):
+        return None
+
