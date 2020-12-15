ecb6f1a852bcfaa09dc28c2c3212bd3fb4f9a561
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-12-15 20:17:27 +0100

    Removed data added download from URL.

--- data_sources/CUZK_DATA50_ChraneneUzemi/source.py
+++ data_sources/CUZK_DATA50_ChraneneUzemi/source.py
@@ -1,11 +1,19 @@
 from .. source import Source
 import os
 from qgis.core import QgsVectorLayer, QgsMessageLog
+import urllib.request
 
 class CUZKCHU(Source):
 
+    def download_data(self):
+        url = 'http://geoportal.cuzk.cz/ZAKAZKY/Data50/hraniceUzemnichJednotek.zip'
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'hraniceUzemnichJednotek.zip')
+        if not os.path.exists(path):
+            urllib.request.urlretrieve(url, path)
+
     def get_vector(self, extent, EPSG):
-        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'ChraneneUzemi.shp')
+        self.download_data()
+        path = '/vsizip/' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data', 'hraniceUzemnichJednotek.zip') + '/hraniceUzemnichJednotek/ChraneneUzemi.shp'
         # TODO read data from HTTP source join with geodata, etc.
         vector = QgsVectorLayer(path, "Chráněné území", "ogr")
         vector.loadNamedStyle(os.path.dirname(__file__) + '/data/style.qml')
