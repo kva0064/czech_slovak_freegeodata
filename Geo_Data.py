e754c5fd07c55b3bd962aa2307ef539f65532919
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-12-18 16:08:26 +0100

    Changed icon.

--- Geo_Data.py
+++ Geo_Data.py
@@ -161,7 +161,8 @@ class GeoData:
     def initGui(self):
         """Create the menu entries and toolbar icons inside the QGIS GUI."""
         
-        icon_path = ':/plugins/Geo_Data/icon.png'
+        icon_path = os.path.join(
+            os.path.dirname(__file__), 'icon.png')
         self.add_action(
             icon_path,
             text=self.tr(u'Browse data sources'),
